7d0e896c81a7c8e0180c2135ac496ff6f5c337c7
shrawnh <fy19aama@leeds.ac.uk>
2024-02-12 01:43:33 +0000

    added enable sensor method

--- src/simulation/controllers/sarsa/_robot.py
+++ src/simulation/controllers/sarsa/_robot.py
@@ -1,29 +1,38 @@
 from controller import Robot, device
 
-
 class MyRobot:
     def __init__(self, robot_instance: Robot) -> None:
         self.robot_instance = robot_instance
         self.time_step = 64
         self.max_speed = 6.28
         self.turn_speed = 0.5
-        self.right_motor_device = robot_instance.getDevice("right wheel motor")
-        self.left_motor_device = robot_instance.getDevice("left wheel motor")
-        self.right_motor_device.setPosition(float("inf"))
-        self.left_motor_device.setPosition(float("inf"))
-        self.right_motor_device.setVelocity(0.0)
-        self.left_motor_device.setVelocity(0.0)
-        self.right_speed: device.Device = 0
-        self.left_speed: device.Device = 0
+        self.right_motor_device: device.Device = robot_instance.getDevice("right wheel motor")
+        self.left_motor_device: device.Device = robot_instance.getDevice("left wheel motor")
+        self.right_speed = 0
+        self.left_speed = 0
+        self.distance_sensors: list[device.Device] = []
         self.dsValues = []  # distance sensors' values
         self.dsNames = ["ds0", "ds1", "ds2", "ds3", "ds12", "ds13", "ds14", "ds15"]
         self.detection_threshold = 180.0
+        self._initialize_motors()
+
+    def _initialize_motors(self) -> None:
+        self.left_motor_device.setPosition(float("inf"))
+        self.right_motor_device.setPosition(float("inf"))
+        self.left_motor_device.setVelocity(0.0)
+        self.right_motor_device.setVelocity(0.0)
 
     def _set_speed(self, left_speed: float, right_speed: float) -> None:
         self.left_motor_device.setVelocity(left_speed)
         self.right_motor_device.setVelocity(right_speed)
         self.right_speed = right_speed
         self.left_speed = left_speed
+
+    def enable_sensors(self) -> None:
+        for name in self.dsNames:
+            sensor = self.robot_instance.getDevice(name)
+            sensor.enable(self.time_step)
+            self.distance_sensors.append(sensor)
 
     def get_speed(self) -> tuple[float, float]:
         return self.left_speed, self.right_speed
@@ -52,10 +61,8 @@ class MyRobot:
         )
 
     def obstacle_detected(self) -> bool:
-        self.dsValues = [self.dsValues[i].getValue() for i in range(len(self.dsNames))]
+        self._enable_sensors()
+        self.dsValues = [
+            self.distance_sensors[i].getValue() for i in range(len(self.dsNames))
+        ]
         return any(dsValue > self.detection_threshold for dsValue in self.dsValues)
-
-    def enable_sensors(self) -> None:
-        for name in self.dsNames:
-            sensor = self.robot_instance.getDevice(name)
-            sensor.enable(self.time_step)
