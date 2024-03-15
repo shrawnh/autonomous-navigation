36e3dd1b943707f42760e0a9187bfa785c75013f
shrawnh <fy19aama@leeds.ac.uk>
2024-03-15 06:39:35 +0000

    saved a model

--- src/simulation/controllers/ppo/ppo.py
+++ src/simulation/controllers/ppo/ppo.py
@@ -14,21 +14,37 @@ def main():
     # Initialize the environment
     env = WheeledRobotEnv(goal, wooden_boxes_data)
     check_env(env, warn=True)
-    model = PPO("MlpPolicy", env, n_steps=2048, verbose=1)
-    model.learn(total_timesteps=1e5)
+
+    # Train the model
+    # model = PPO("MlpPolicy", env, n_steps=2048, verbose=1)
+    # model.learn(total_timesteps=1e5)
+
+    # Save the trained model
     # model.save("ppo_wheeled_robot")
 
+    # Load the trained model
+    model = PPO.load("ppo_wheeled_robot")
+
     # Replay
-    print("Training is finished, press `Y` for replay...")
+    print("Training is finished, press `A` for replay...")
     env.wait_keyboard()
 
-    obs = env.reset()
-    for _ in range(10):
-        action, _states = model.predict(obs)
-        obs, reward, done, info = env.step(action)
-        print(obs, reward, done, info)
+    # Use the model to make predictions
+    observation, _ = env.reset()
+    env.keyboard.enable(env.timestep)
+    while env.keyboard.getKey() != ord("S"):
+        action, _states = model.predict(observation, deterministic=True)
+        observation, reward, done, truncated, info = env.step(action)
+        print(f"Observation: {observation}")
+        print(f"Reward: {reward}")
+        print(f"Done: {done}")
+        print(f"Truncated: {truncated}")
+        print(f"Info: {info}")
         if done:
-            obs = env.reset()
+            observation, _ = env.reset()
+
+    env.keyboard.disable()
+    env.reset()
 
 
 if __name__ == "__main__":
