b2f264df22d29c913fe128b7c6df3517a341c983
shrawnh <fy19aama@leeds.ac.uk>
2024-03-15 22:36:08 +0000

    created a2c controller

---
+++ src/simulation/controllers/a2c/a2c.py
@@ -0,0 +1,39 @@
+from myenv.myenv import WheeledRobotEnv, run_model, get_env_data_from_config
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_checker import check_env
+
+# train / train_save / test
+MODEL_MODE = "train"
+
+# easy / medium / hard
+ENV_MODE = "easy"
+
+# front / front_back / sides
+ROBOT_SENSORS = "front"
+
+
+def main():
+
+    goal, wooden_boxes_data, grid_size, robot_sensors = get_env_data_from_config(
+        ENV_MODE, MODEL_MODE.split("_")[0], ROBOT_SENSORS
+    )
+    env = WheeledRobotEnv(goal, wooden_boxes_data, grid_size, robot_sensors)
+    check_env(env, warn=True)
+
+    if MODEL_MODE == "train":
+        model = A2C("MlpPolicy", env, n_steps=2048, verbose=1)
+        model.learn(total_timesteps=1e5)
+
+    elif MODEL_MODE == "train-save":
+        model = A2C("MlpPolicy", env, n_steps=2048, verbose=1)
+        model.learn(total_timesteps=1e5)
+        model.save("ppo_wheeled_robot")
+
+    elif MODEL_MODE == "test":
+        model = A2C.load("ppo_wheeled_robot")
+
+    run_model(env, model)
+
+
+if __name__ == "__main__":
+    main()
