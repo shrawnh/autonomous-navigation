ae270871c6a402eac42ca16ec3b49414bed0f77e
shrawnh <fy19aama@leeds.ac.uk>
2024-03-28 07:54:31 +0000

    fixed toml not top level bug in create config

--- utils/create_configs.py
+++ utils/create_configs.py
@@ -6,55 +6,83 @@ configs_dir = "/Users/shrwnh/Development/autonomous-navigation/src/simulation/co
 
 os.makedirs(configs_dir, exist_ok=True)
 
-for filename in os.listdir(worlds_dir):
-    if filename.endswith(".wbt"):
-        # create mode file depending on the file name if it contains test or train, then mode is test or train
-        if "test" in filename:
-            mode = "test"
-        elif "train" in filename:
-            mode = "train"
-        else:
-            print(
-                f"\033[91mSkipping {filename} because it doesn't contain either test or train\033[0m"
-            )
-            continue
-        if os.path.exists(os.path.join(configs_dir, os.path.splitext(filename)[0])):
-            print(f"\033[92mUpdated {os.path.splitext(filename)[0]} {mode}.toml\033[0m")
-            dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
-        else:
-            print(f"\033[93mCreated {os.path.splitext(filename)[0]} {mode}.toml\033[0m")
-            dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
-            os.makedirs(dir_name, exist_ok=True)
 
-        toml_file = os.path.join(dir_name, f"{mode}.toml")
+def create_configs():
+    for filename in os.listdir(worlds_dir):
+        if filename.endswith(".wbt"):
+            # create mode file depending on the file name if it contains test or train, then mode is test or train
+            if "test" in filename:
+                mode = "test"
+            elif "train" in filename:
+                mode = "train"
+            else:
+                print(
+                    f"\033[91mSkipping {filename} because it doesn't contain either test or train\033[0m"
+                )
+                continue
+            if os.path.exists(os.path.join(configs_dir, os.path.splitext(filename)[0])):
+                print(
+                    f"\033[92mUpdated {os.path.splitext(filename)[0]} {mode}.toml\033[0m"
+                )
+                dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
+            else:
+                print(
+                    f"\033[93mCreated {os.path.splitext(filename)[0]} {mode}.toml\033[0m"
+                )
+                dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
+                os.makedirs(dir_name, exist_ok=True)
 
-        with open(toml_file, "w") as f:
-            with open(os.path.join(worlds_dir, filename)) as file:
-                lines = iter(file)
-                wooden_box_exist = False
-                for line in lines:
-                    if "DEF WB" in line:
-                        wooden_box = line.split()[1]
-                        position_line = next(lines)
-                        position = [float(i) for i in position_line.split()[1:4]]
-                        not wooden_box_exist and f.write(f"\n[wooden_boxes]\n")
-                        f.write(f"{wooden_box}.name = '{wooden_box}'\n")
-                        f.write(f"{wooden_box}.position = {position}\n")
-                        wooden_box_exist = True
-                    elif "DEF PIONEER2" in line:
-                        robot = line.split()[1]
-                        position_line = next(lines)
-                        position = [float(i) for i in position_line.split()[1:4]]
-                        f.write(f"\n[robot]\n")
-                        f.write(f"{robot}.name = '{robot}'\n")
-                        f.write(f"{robot}.position = {position}\n")
-                    elif "DEF GoalSquare" in line:
-                        position_line = next(lines)
-                        goal = [float(i) for i in position_line.split()[1:4]]
-                        f.write(f"\ngoal = {goal}\n")
-                    elif "floorSize" in line:
-                        grid_size = float(line.split()[1]) * float(line.split()[2])
-                        f.write(f"\ngrid_size = {grid_size}\n")
-                        f.write(
-                            f"\ngrid = [{float(line.split()[1])}, {float(line.split()[2])}]\n"
-                        )
+            toml_file = os.path.join(dir_name, f"{mode}.toml")
+
+            with open(toml_file, "w") as f:
+                line_array = []
+                with open(os.path.join(worlds_dir, filename)) as file:
+                    lines = iter(file)
+                    wooden_box_exist = False
+                    for line in lines:
+                        if "DEF WB" in line:
+                            wooden_box = line.split()[1]
+                            position_line = next(lines)
+                            position = [float(i) for i in position_line.split()[1:4]]
+                            # not wooden_box_exist and f.write(f"\n[wooden_boxes]\n")
+                            # f.write(f"{wooden_box}.name = '{wooden_box}'\n")
+                            # f.write(f"{wooden_box}.position = {position}\n")
+                            not wooden_box_exist and line_array.append(
+                                f"\n[wooden_boxes]\n"
+                            )
+                            line_array.append(f"{wooden_box}.name = '{wooden_box}'\n")
+                            line_array.append(f"{wooden_box}.position = {position}\n")
+                            wooden_box_exist = True
+                        elif "DEF PIONEER2" in line:
+                            robot = line.split()[1]
+                            position_line = next(lines)
+                            position = [float(i) for i in position_line.split()[1:4]]
+                            # f.write(f"\n[robot]\n")
+                            # f.write(f"{robot}.name = '{robot}'\n")
+                            # f.write(f"{robot}.position = {position}\n")
+                            line_array.append(f"\n[robot]\n")
+                            line_array.append(f"{robot}.name = '{robot}'\n")
+                            line_array.append(f"{robot}.position = {position}\n")
+                        elif "DEF GoalSquare" in line:
+                            position_line = next(lines)
+                            goal = [float(i) for i in position_line.split()[1:4]]
+                            # f.write(f"\ngoal = {goal}\n")
+                            line_array.insert(0, f"\ngoal = {goal}\n")
+                        elif "floorSize" in line:
+                            grid_size = float(line.split()[1]) * float(line.split()[2])
+                            # f.write(f"\ngrid_size = {grid_size}\n")
+                            # f.write(
+                            #     f"\ngrid = [{float(line.split()[1])}, {float(line.split()[2])}]\n"
+                            # )
+                            line_array.append(f"\ngrid_size = {grid_size}\n")
+                            line_array.append(
+                                f"\ngrid = [{float(line.split()[1])}, {float(line.split()[2])}]\n"
+                            )
+
+                # this is to write the lines in specified order
+                f.writelines(line_array)
+
+
+if "__main__" == __name__:
+    create_configs()
+    print("Done creating configs.")
