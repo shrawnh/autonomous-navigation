6a48e3fb67ab44cb48fd73da17e04a9860951dd3
shrawnh <fy19aama@leeds.ac.uk>
2024-03-30 03:43:23 +0000

    updated world and configs

--- utils/create_configs.py
+++ utils/create_configs.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 worlds_dir = "/Users/shrwnh/Development/autonomous-navigation/src/simulation/worlds"
 
@@ -7,7 +8,14 @@ configs_dir = "/Users/shrwnh/Development/autonomous-navigation/src/simulation/co
 os.makedirs(configs_dir, exist_ok=True)
 
 
+def get_file_hash(file_path):
+    with open(file_path, "rb") as f:
+        return hashlib.md5(f.read()).hexdigest()
+
+
 def create_configs():
+    num_of_changes = 0
+    files_checked = 0
     for filename in os.listdir(worlds_dir):
         if filename.endswith(".wbt"):
             # create mode file depending on the file name if it contains test or train, then mode is test or train
@@ -21,9 +29,6 @@ def create_configs():
                 )
                 continue
             if os.path.exists(os.path.join(configs_dir, os.path.splitext(filename)[0])):
-                print(
-                    f"\033[92mUpdated {os.path.splitext(filename)[0]} {mode}.toml\033[0m"
-                )
                 dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
             else:
                 print(
@@ -32,7 +37,9 @@ def create_configs():
                 dir_name = os.path.join(configs_dir, os.path.splitext(filename)[0])
                 os.makedirs(dir_name, exist_ok=True)
 
+            files_checked += 1
             toml_file = os.path.join(dir_name, f"{mode}.toml")
+            before_hash = get_file_hash(toml_file)
 
             with open(toml_file, "w") as f:
                 line_array = []
@@ -44,9 +51,6 @@ def create_configs():
                             wooden_box = line.split()[1]
                             position_line = next(lines)
                             position = [float(i) for i in position_line.split()[1:4]]
-                            # not wooden_box_exist and f.write(f"\n[wooden_boxes]\n")
-                            # f.write(f"{wooden_box}.name = '{wooden_box}'\n")
-                            # f.write(f"{wooden_box}.position = {position}\n")
                             not wooden_box_exist and line_array.append(
                                 f"\n[wooden_boxes]\n"
                             )
@@ -57,32 +61,32 @@ def create_configs():
                             robot = line.split()[1]
                             position_line = next(lines)
                             position = [float(i) for i in position_line.split()[1:4]]
-                            # f.write(f"\n[robot]\n")
-                            # f.write(f"{robot}.name = '{robot}'\n")
-                            # f.write(f"{robot}.position = {position}\n")
                             line_array.append(f"\n[robot]\n")
                             line_array.append(f"{robot}.name = '{robot}'\n")
                             line_array.append(f"{robot}.position = {position}\n")
                         elif "DEF GoalSquare" in line:
                             position_line = next(lines)
                             goal = [float(i) for i in position_line.split()[1:4]]
-                            # f.write(f"\ngoal = {goal}\n")
                             line_array.insert(0, f"\ngoal = {goal}\n")
                         elif "floorSize" in line:
-                            grid_size = float(line.split()[1]) * float(line.split()[2])
-                            # f.write(f"\ngrid_size = {grid_size}\n")
-                            # f.write(
-                            #     f"\ngrid = [{float(line.split()[1])}, {float(line.split()[2])}]\n"
-                            # )
-                            line_array.append(f"\ngrid_size = {grid_size}\n")
                             line_array.append(
-                                f"\ngrid = [{float(line.split()[1])}, {float(line.split()[2])}]\n"
+                                f"\ngrid_size = {float(line.split()[1])}\n"
                             )
 
                 # this is to write the lines in specified order
                 f.writelines(line_array)
 
+            after_hash = get_file_hash(toml_file)
+
+            if before_hash != after_hash:
+                num_of_changes += 1
+                print(
+                    f"\033[92mUpdated {os.path.splitext(filename)[0]} {mode}.toml\033[0m"
+                )
+
+    print(f"Files checked: {files_checked}")
+    print(f"Number of changes: {num_of_changes}")
+
 
 if "__main__" == __name__:
     create_configs()
-    print("Done creating configs.")
